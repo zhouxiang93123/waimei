11e00ca18f79c871c2d111e7ec42e6671553f0f4
be4-news <be4-news@mailfence.com>
2020-08-03 13:28:45 +0000

    CNA

--- perform.js
+++ perform.js
@@ -14,7 +14,7 @@ let feedxUrls = {
   '德国之声': 'https://feedx.net/rss/dw.xml',
   '联合早报': 'https://rsshub.app/zaobao/realtime/china',
   'RTI 中央廣播電臺': 'http://www.rti.org.tw/rss/',
-  '中央社CNA': 'https://feedx.net/rss/cna.xml',
+  '(臺)中央社CNA': 'https://feedx.net/rss/cna.xml',
   'CCTV新闻联播': 'https://rsshub.app/xinwenlianbo/index',
   '维权网': 'https://wqw2010.blogspot.com/feeds/posts/default',
   '寒冬': 'https://zh.bitterwinter.org/feed/',
